9d39b83369e2635ad16513a8c44bf3238d65366d
Andreas Alin <andreas.alin@finatech.se>
2017-09-03 15:02:07 +0200

    Use StructReader

--- src/load_chunks.js
+++ src/load_chunks.js
@@ -1,4 +1,4 @@
-import BinaryBuffer from './binary_buffer';
+import BinaryBuffer, { StructReader } from './binary_buffer';
 
 function assert(condition, message) {
   if (!condition) {
@@ -6,24 +6,28 @@ function assert(condition, message) {
   }
 }
 
+const FileHeader = new StructReader({
+  hdr: ['String', 4],
+  version: '16LE'
+});
+
+const ChunkHeader = new StructReader({
+  type: ['String', 4],
+  size: '32LE'
+})
+
 export default
 function* loadChunks(data, header, version, callback) {
   const buffer = new BinaryBuffer(data);
   let style = {};
 
-  const fileHeader = {
-    hdr: buffer.readString(4),
-    version: buffer.read16()
-  };
+  const fileHeader = buffer.readStruct(FileHeader);
 
   assert(fileHeader.hdr === header, `Wrong header: ${fileHeader.hdr}`);
   assert(fileHeader.version === version, `Wrong version: ${fileHeader.version}`);
 
   while (!buffer.eof()) {
-    const chunkHeader = {
-      type: buffer.readString(4),
-      size: buffer.read32()
-    };
+    const chunkHeader = buffer.readStruct(ChunkHeader);
 
     if (!/^[A-Z]{4}$/.test(chunkHeader.type)) {
       throw "chunk header mangled, bad position maybe?";
